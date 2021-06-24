ae2efb58734ea1350b339b2a59a4858bfb682265
artem <artemza1tsev@hotmail.com>
2021-06-24 14:03:40 +0300

    add start

--- index.js
+++ index.js
@@ -10,27 +10,37 @@ for(i in product_read) {
     /* console.log(product_read.length); */
 };
 
-
-
 const token = "1850230832:AAED80DMUF-tcR50VP252HRaOh0mCAu02hM"
-
 const bot = new TelegramApi(token, {polling: true})
-
 
 
 bot.setMyCommands( [
     {command: '/start', description: 'Начальное приветствие'},
-    {command: '/info', description: 'Получить информацию о пользователе'},
-])
+    ])
 
 bot.on('message',  msg => {  
-    const text = msg.text;
+    const newStr = msg.text;
     const chatId = msg.chat.id;
+    if (newStr === '/start') {
+                
+                 bot.sendPhoto(chatId, 'https://i.ibb.co/YdwprZS/kisspng-globus-sb-warenhaus-sankt-wendel-retail-supermarke-globus-sb-warenhaus-holding-gmbh-amp-co-k.png');
+               return bot.sendMessage(chatId, `Добро пожаловать в телеграм бот поиска продуктов в гипермаркете Глобус. Напиши название товара и я попробую его найти.`);
+            };
+
+    
+  
+    const text = newStr.toLowerCase();
+   
+    const oneUpper = text.charAt(0).toUpperCase() + text.slice(1);
+    
+   
    
  for (var i=0, len=product_read.length; i<len; i++) {
-    if (product_read[i] === text)
+    if (product_read[i] === oneUpper)
         return bot.sendPhoto(chatId, `${links_read[i]}`)
       };
+
+      return bot.sendMessage(chatId, 'Товар не найден.');
 })
 
 
